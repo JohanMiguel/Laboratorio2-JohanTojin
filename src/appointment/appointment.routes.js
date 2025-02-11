b6943d242ad3275c667d1bfbecf1a251d4d145c9
JohanMiguel <jtojin-2020591@kinal.edu.gt>
2025-02-10 23:23:15 -0600

    feat(AdoptionSystem): se modifica la ruta de actualizacion

--- src/appointment/appointment.routes.js
+++ src/appointment/appointment.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { saveAppointment, getAppoiment, actualizarAppoinment, cancelarAppointment } from "./appointment.controller.js";
-import { createAppointmentValidator, updateAppointmentValidator } from "../middlewares/appointment-validators.js";
+import { createAppointmentValidator } from "../middlewares/appointment-validators.js";
 
 const router = Router();
 
 // Ruta para crear cita
 router.post("/createAppointment", createAppointmentValidator, saveAppointment);
 router.get("/", getAppoiment);
-router.put("/actualizarAppoinment/:uid", updateAppointmentValidator, actualizarAppoinment);
+router.put("/actualizarAppoinment/:uid", createAppointmentValidator, actualizarAppoinment);
 router.patch("/cancelarAppoinment/:uid", cancelarAppointment);
 export default router;
